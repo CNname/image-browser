595ed88e503069b314d651806c735c6819decc76
CNname <ari.aittomaki@protonmail.com>
2022-04-09 22:40:22 +0300

    chore(config): add basic CSP

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  default-src 'none';
+  script-src 'self' 'unsafe-eval';
+  frame-ancestors 'none';
+  style-src 'unsafe-inline';
+  font-src 'self';
+  img-src 'self' https://via.placeholder.com data:;
+  connect-src 'self' https://jsonplaceholder.typicode.com/;
+`;
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+];
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -11,6 +29,15 @@ const nextConfig = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
